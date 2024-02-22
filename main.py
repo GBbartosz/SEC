5f166cce9a373b7ffa808c0d7001b0b083c7ab07
GBbartosz <bartosz.grygalewicz@gmail.com>
2024-02-22 02:09:20 +0100

    2024-02-21

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+a = [1,4]
+b = [1,2,3]
+if all(elem in b for elem in a):
+    print('true')
+else:
+    print('false')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- secdownload.py
+++ secdownload.py
@@ -1,5 +1,8 @@
 import requests
 import pandas as pd
+import datetime
+
+from matplotlib import pyplot as plt
 
 
 def pandas_df_display_options():
@@ -11,8 +14,12 @@ def pandas_df_display_options():
 
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_colwidth', 20)
+    pd.set_option('display.max_colwidth', 40)
     pd.set_option('display.width', 400)
+
+
+def get_quarter(x):
+    return x[-2:] if pd.isna(x) is False and 'Q' in x else None
 
 
 pandas_df_display_options()
@@ -48,8 +55,60 @@ for i in tickers_df.index[[0]]:
     print(facts['facts']['us-gaap']['NetIncomeLoss']['units']['USD'])
 
     timel = facts['facts']['us-gaap']['NetIncomeLoss']['units']['USD']
-    for t in timel:
-        start = t['start']
-        end = t['end']
-        val = t['val']
-        print(f'{start}-{end}: {val}')
+
+    df = pd.DataFrame(timel)
+
+    df['start'] = pd.to_datetime(df['start'])
+    df['end'] = pd.to_datetime(df['end'])
+
+    df = df.dropna(subset='frame')
+    df['year'] = df['end'].dt.year
+    df['quarter'] = df['frame'].apply(get_quarter)
+    df = df.drop(['accn', 'fy', 'fp', 'form', 'filed', 'frame'], axis=1)
+
+    years = sorted(df['year'].unique())
+    first_year = min(years)
+    years = years[1:]
+    df = df[df['year'] > first_year]
+    df = df.sort_values(by=['end'])
+    df = df.reset_index(drop=True)
+
+    indexes_to_update = []
+    for year in years:
+        ydf = df[df['year'] == year]
+        if all(q in ydf['quarter'].values for q in ['Q1', 'Q2', 'Q3', 'Q4']) is False:
+            indexes_to_update.append(ydf[ydf['quarter'].isna()].index[0])
+
+    for i in indexes_to_update:
+        start = df.iloc[i - 1]['end'] + datetime.timedelta(days=1)
+        end = df.iloc[i]['end']
+
+        year_value = df.iloc[i]['val']
+        prev_q1 = df.iloc[i - 1]['val']
+        prev_q2 = df.iloc[i - 2]['val']
+        prev_q3 = df.iloc[i - 3]['val']
+        lacking_quarter_val = year_value - prev_q1 - prev_q2 - prev_q3
+
+        year = df.iloc[i]['year']
+        prev_q = df.iloc[i - 1]['quarter']
+        quarter = f'Q{int(prev_q[-1]) + 1}'
+
+        new_row = [start, end, lacking_quarter_val, year, quarter]
+        df.iloc[i, :] = new_row
+
+    df = df.sort_values(by=['end'])
+    df = df.dropna().reset_index(drop=True)
+    print(df)
+
+    plt.figure()
+    plt.plot(df['end'], df['val'])
+    plt.show()
+
+    #for t in timel:
+    #    form = t['form']
+    #    start = t['start']
+    #    end = t['end']
+    #    val = t['val']
+    #    print(f'{form}: {start}-{end}: {val}')
+
+        # 44161000000
