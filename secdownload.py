657154abf8d5ed52ca601e7ece3ab8b13404b524
GBbartosz <bartosz.grygalewicz@gmail.com>
2024-02-23 21:14:07 +0100

    2024-02-23

--- secdownload.py
+++ secdownload.py
@@ -1,4 +1,5 @@
 import requests
+import numpy as np
 import pandas as pd
 import datetime
 
@@ -36,38 +37,47 @@ for i in tickers_df.index[[0]]:
     cik = tickers_df['cik_str'][i]
     ticker = tickers_df['ticker'][i]
     company_name = tickers_df['title'][i]
-    print(cik)
     print(ticker)
-    print(company_name)
     #metadata = requests.get(f'https://data.sec.gov/submissions/CIK{cik}.json', headers=headers).json()
     #print(metadata)
     #print()
     #print(metadata.keys())
     facts = requests.get(f'https://data.sec.gov/api/xbrl/companyfacts/CIK{cik}.json', headers=headers).json()
-    print(facts.keys())
-    print(facts['facts']['us-gaap'].keys())
-    print(facts['facts']['dei'].keys())
-    print()
-    print(facts['facts']['us-gaap']['NetIncomeLoss'])
-    print(facts['facts']['dei']['EntityCommonStockSharesOutstanding'])
+    #print(facts.keys())
+    #print(facts['facts']['us-gaap'].keys())
+    #print(facts['facts']['dei'].keys())
+    #print()
+    #print(facts['facts']['us-gaap']['NetIncomeLoss'])
+    #print(facts['facts']['dei']['EntityCommonStockSharesOutstanding'])
     #print(facts['facts']['dei']['EntityListingParValuePerShare'])
-    print()
-    print(facts['facts']['us-gaap']['NetIncomeLoss'].keys())
-    print(facts['facts']['us-gaap']['NetIncomeLoss']['units'])
-    print(facts['facts']['us-gaap']['NetIncomeLoss']['units']['USD'])
+    #print()
+    print(facts['facts']['us-gaap']['CommonStockSharesOutstanding'])
+    #not_summarizing_indicators = []
+    summarizing_indicators = ['Revenues', 'NetIncomeLoss']
+    #indicators = not_summarizing_indicators + summarizing_indicators
+    indicators = summarizing_indicators
+    df = None
+    for indicator in indicators:
+        inddf = pd.DataFrame(facts['facts']['us-gaap'][indicator]['units']['USD'])
+        inddf = inddf.rename(columns={'val': indicator})
+        inddf.to_excel(f'{main_folder_path}_ind_tst_{indicator}.xlsx')
+        if df is None:
+            inddf = inddf.reindex(columns=[col for col in inddf.columns if col != indicator] + [indicator])  # move indicator column to last position
+            df = inddf
+        else:
+            df = pd.merge(df, inddf, on=['start', 'end', 'accn', 'fy', 'fp', 'form', 'filed', 'frame'])
 
-    timel = facts['facts']['us-gaap']['NetIncomeLoss']['units']['USD']
-
-    df = pd.DataFrame(timel)
-
+    #print(df.tail(1000))
     df['start'] = pd.to_datetime(df['start'])
     df['end'] = pd.to_datetime(df['end'])
 
     df = df.dropna(subset='frame')
     df['year'] = df['end'].dt.year
     df['quarter'] = df['frame'].apply(get_quarter)
-    print(df)
-    df = df.drop(['accn', 'fy', 'fp', 'form', 'filed', 'frame'], axis=1)
+    df = df.drop(['accn', 'fy', 'fp', 'form', 'frame'], axis=1)
+
+    base_columns = ['start', 'end', 'filed', 'year', 'quarter']
+    df = df.reindex(columns=base_columns + indicators)
 
     years = sorted(df['year'].unique())
     first_year = min(years)
@@ -75,7 +85,7 @@ for i in tickers_df.index[[0]]:
     df = df[df['year'] > first_year]
     df = df.sort_values(by=['end'])
     df = df.reset_index(drop=True)
-
+    print(df)
     indexes_to_update = []
     for year in years:
         ydf = df[df['year'] == year]
@@ -84,30 +94,44 @@ for i in tickers_df.index[[0]]:
             if ind >= 3:  # nie mozna odjac poprzednich kwartalow
                 indexes_to_update.append(ind)
 
+    #first_not_summarizing_indicator_column = not_summarizing_indicators[0]
+    #last_not_summarizing_indicator_column = not_summarizing_indicators[-1]
+    first_summarizing_indicator_column = summarizing_indicators[0]
+    last_summarizing_indicator_column = summarizing_indicators[-1]
     for i in indexes_to_update:
         start = df.iloc[i - 1]['end'] + datetime.timedelta(days=1)
         end = df.iloc[i]['end']
+        filed = df.iloc[i]['filed']
+        #not_summarizing_vals = [df.iloc[i][first_not_summarizing_indicator_column:last_not_summarizing_indicator_column][0]]   # zmienic po dodaniu kolejnych wskaznikow do not summarizing
+        #not_summarizing_vals = list(df.iloc[i][first_not_summarizing_indicator_column:last_not_summarizing_indicator_column])
 
-        year_value = df.iloc[i]['val']
-        prev_q1 = df.iloc[i - 1]['val']
-        prev_q2 = df.iloc[i - 2]['val']
-        prev_q3 = df.iloc[i - 3]['val']
-        lacking_quarter_val = year_value - prev_q1 - prev_q2 - prev_q3
+        year_value = np.array(df.iloc[i][first_summarizing_indicator_column:last_summarizing_indicator_column])
+        prev_q1 = np.array(df.iloc[i - 1][first_summarizing_indicator_column:last_summarizing_indicator_column])
+        prev_q2 = np.array(df.iloc[i - 2][first_summarizing_indicator_column:last_summarizing_indicator_column])
+        prev_q3 = np.array(df.iloc[i - 3][first_summarizing_indicator_column:last_summarizing_indicator_column])
+
+        #year_value = df.iloc[i]['val']
+        #prev_q1 = df.iloc[i - 1]['val']
+        #prev_q2 = df.iloc[i - 2]['val']
+        #prev_q3 = df.iloc[i - 3]['val']
+        lacking_quarter_val = list(year_value - prev_q1 - prev_q2 - prev_q3)
 
         year = df.iloc[i]['year']
         prev_q = df.iloc[i - 1]['quarter']
         quarter = f'Q{int(prev_q[-1]) + 1}'
 
-        new_row = [start, end, lacking_quarter_val, year, quarter]
+        #new_row = [start, end, filed, year, quarter] + not_summarizing_vals + lacking_quarter_val
+        new_row = [start, end, filed, year, quarter] + lacking_quarter_val
         df.iloc[i, :] = new_row
 
     df = df.sort_values(by=['end'])
     df = df.dropna().reset_index(drop=True)
-    print(df)
+    print(df.tail(5))
+    df.to_csv(f'{main_folder_path}metrics\\{ticker}_metrics.csv')
 
-    plt.figure()
-    plt.plot(df['end'], df['val'])
-    plt.show()
+    #plt.figure()
+    #plt.plot(df['end'], df['CommonStockSharesOutstanding'])
+    #plt.show()
 
     #for t in timel:
     #    form = t['form']
