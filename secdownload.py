d34dc75553de35c7d216fea48a79766d41a91973
GBbartosz <bartosz.grygalewicz@gmail.com>
2024-02-24 14:01:34 +0100

    2024-02-24

--- secdownload.py
+++ secdownload.py
@@ -5,6 +5,9 @@ import datetime
 
 from matplotlib import pyplot as plt
 
+# class with parameters for each indicator
+# divide by 10000000
+
 
 def pandas_df_display_options():
     pd.reset_option('display.max_rows')
@@ -56,82 +59,92 @@ for i in tickers_df.index[[0]]:
     summarizing_indicators = ['Revenues', 'NetIncomeLoss']
     #indicators = not_summarizing_indicators + summarizing_indicators
     indicators = summarizing_indicators
-    df = None
-    for indicator in indicators:
-        inddf = pd.DataFrame(facts['facts']['us-gaap'][indicator]['units']['USD'])
-        inddf = inddf.rename(columns={'val': indicator})
-        inddf.to_excel(f'{main_folder_path}_ind_tst_{indicator}.xlsx')
-        if df is None:
-            inddf = inddf.reindex(columns=[col for col in inddf.columns if col != indicator] + [indicator])  # move indicator column to last position
-            df = inddf
-        else:
-            df = pd.merge(df, inddf, on=['start', 'end', 'accn', 'fy', 'fp', 'form', 'filed', 'frame'])
-
-    #print(df.tail(1000))
-    df['start'] = pd.to_datetime(df['start'])
-    df['end'] = pd.to_datetime(df['end'])
-
-    df = df.dropna(subset='frame')
-    df['year'] = df['end'].dt.year
-    df['quarter'] = df['frame'].apply(get_quarter)
-    df = df.drop(['accn', 'fy', 'fp', 'form', 'frame'], axis=1)
-
     base_columns = ['start', 'end', 'filed', 'year', 'quarter']
-    df = df.reindex(columns=base_columns + indicators)
+    total_df = None
+    for indicator in indicators:
+        df = pd.DataFrame(facts['facts']['us-gaap'][indicator]['units']['USD'])
+        df = df.rename(columns={'val': indicator})
+        #inddf.to_excel(f'{main_folder_path}_ind_tst_{indicator}.xlsx')
 
-    years = sorted(df['year'].unique())
-    first_year = min(years)
-    years = years[1:]
-    df = df[df['year'] > first_year]
-    df = df.sort_values(by=['end'])
-    df = df.reset_index(drop=True)
-    print(df)
-    indexes_to_update = []
-    for year in years:
-        ydf = df[df['year'] == year]
-        if all(q in ydf['quarter'].values for q in ['Q1', 'Q2', 'Q3', 'Q4']) is False:
-            ind = ydf[ydf['quarter'].isna()].index[0]
-            if ind >= 3:  # nie mozna odjac poprzednich kwartalow
-                indexes_to_update.append(ind)
 
-    #first_not_summarizing_indicator_column = not_summarizing_indicators[0]
-    #last_not_summarizing_indicator_column = not_summarizing_indicators[-1]
-    first_summarizing_indicator_column = summarizing_indicators[0]
-    last_summarizing_indicator_column = summarizing_indicators[-1]
-    for i in indexes_to_update:
-        start = df.iloc[i - 1]['end'] + datetime.timedelta(days=1)
-        end = df.iloc[i]['end']
-        filed = df.iloc[i]['filed']
-        #not_summarizing_vals = [df.iloc[i][first_not_summarizing_indicator_column:last_not_summarizing_indicator_column][0]]   # zmienic po dodaniu kolejnych wskaznikow do not summarizing
-        #not_summarizing_vals = list(df.iloc[i][first_not_summarizing_indicator_column:last_not_summarizing_indicator_column])
+        print(df.sort_values(by='end').tail(1000))
+        df['start'] = pd.to_datetime(df['start'])
+        df['end'] = pd.to_datetime(df['end'])
 
-        year_value = np.array(df.iloc[i][first_summarizing_indicator_column:last_summarizing_indicator_column])
-        prev_q1 = np.array(df.iloc[i - 1][first_summarizing_indicator_column:last_summarizing_indicator_column])
-        prev_q2 = np.array(df.iloc[i - 2][first_summarizing_indicator_column:last_summarizing_indicator_column])
-        prev_q3 = np.array(df.iloc[i - 3][first_summarizing_indicator_column:last_summarizing_indicator_column])
+        df = df.dropna(subset='frame')
+        df['year'] = df['end'].dt.year
+        df['quarter'] = df['frame'].apply(get_quarter)
+        df = df.drop(['accn', 'fy', 'fp', 'form', 'frame'], axis=1)
 
-        #year_value = df.iloc[i]['val']
-        #prev_q1 = df.iloc[i - 1]['val']
-        #prev_q2 = df.iloc[i - 2]['val']
-        #prev_q3 = df.iloc[i - 3]['val']
-        lacking_quarter_val = list(year_value - prev_q1 - prev_q2 - prev_q3)
+        years = sorted(df['year'].unique())
+        first_year = min(years)
+        years = years[1:]
+        df = df[df['year'] > first_year]
+        df = df.sort_values(by=['end'])
+        df = df.reset_index(drop=True)
+        print(df)
+        indexes_to_update = []
+        for year in years:
+            ydf = df[df['year'] == year]
+            if all(q in ydf['quarter'].values for q in ['Q1', 'Q2', 'Q3', 'Q4']) is False:
+                try:
+                    ind = ydf[ydf['quarter'].isna()].index[0]
+                except IndexError:  # case when there is lacking quarter and there is not full year statement
+                    ind = -1
+                if ind >= 3:  # nie mozna odjac poprzednich kwartalow
+                    indexes_to_update.append(ind)
 
-        year = df.iloc[i]['year']
-        prev_q = df.iloc[i - 1]['quarter']
-        quarter = f'Q{int(prev_q[-1]) + 1}'
+        for i in indexes_to_update:
+            start = df.iloc[i - 1]['end'] + datetime.timedelta(days=1)
+            end = df.iloc[i]['end']
+            filed = df.iloc[i]['filed']
+            #not_summarizing_vals = [df.iloc[i][first_not_summarizing_indicator_column:last_not_summarizing_indicator_column][0]]   # zmienic po dodaniu kolejnych wskaznikow do not summarizing
+            #not_summarizing_vals = list(df.iloc[i][first_not_summarizing_indicator_column:last_not_summarizing_indicator_column])
 
-        #new_row = [start, end, filed, year, quarter] + not_summarizing_vals + lacking_quarter_val
-        new_row = [start, end, filed, year, quarter] + lacking_quarter_val
-        df.iloc[i, :] = new_row
+            year_value = np.array(df.iloc[i][indicator])
+            prev_q1 = np.array(df.iloc[i - 1][indicator])
+            prev_q2 = np.array(df.iloc[i - 2][indicator])
+            prev_q3 = np.array(df.iloc[i - 3][indicator])
 
-    df = df.sort_values(by=['end'])
-    df = df.dropna().reset_index(drop=True)
-    print(df.tail(5))
-    df.to_csv(f'{main_folder_path}metrics\\{ticker}_metrics.csv')
+            #year_value = df.iloc[i]['val']
+            #prev_q1 = df.iloc[i - 1]['val']
+            #prev_q2 = df.iloc[i - 2]['val']
+            #prev_q3 = df.iloc[i - 3]['val']
+            lacking_quarter_val = year_value - prev_q1 - prev_q2 - prev_q3
 
-    #plt.figure()
-    #plt.plot(df['end'], df['CommonStockSharesOutstanding'])
-    #plt.show()
+            year = df.iloc[i]['year']
+            prev_q = df.iloc[i - 1]['quarter']
+            quarter = f'Q{int(prev_q[-1]) + 1}'
+
+            #new_row = [start, end, filed, year, quarter] + not_summarizing_vals + lacking_quarter_val
+            new_row = [start, end, lacking_quarter_val, filed, year, quarter]
+            print('xxx')
+            print(df.head())
+            df.iloc[i, :] = new_row
+
+        df = df.sort_values(by=['end'])
+        df = df.dropna().reset_index(drop=True)
+        df = df.reindex(columns=base_columns + [indicator])
+        print(df.tail(5))
+
+        if total_df is None:
+
+            total_df = df
+        else:
+            total_df = pd.merge(total_df, df, on=['start', 'end', 'filed', 'year', 'quarter'])
+
+    print(total_df.tail(10))
+    total_df.to_csv(f'{main_folder_path}metrics\\{ticker}_metrics.csv')
+
+    plt.figure()
+    plt.plot(total_df['end'], total_df[indicators[0]])
+    plt.title(indicators[0])
+    plt.show()
+
+    plt.figure()
+    plt.plot(total_df['end'], total_df[indicators[1]])
+    plt.title(indicators[1])
+    plt.show()
 
     #for t in timel:
     #    form = t['form']
