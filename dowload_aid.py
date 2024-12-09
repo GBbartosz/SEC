4892e8d58eaf70ddd65b98b6a47a34d223f75795
barto <bartosz.grygalewicz@gmail.com>
2024-12-09 19:25:58 +0100

    2024-12-09

--- dowload_aid.py
+++ dowload_aid.py
@@ -1,18 +1,114 @@
+import csv
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+def get_data_from_table(mydriver):
+    tables = mydriver.find_elements(By.CLASS_NAME, 'historical_data_table')
+    table = tables[1]
+    rows = table.find_elements(By.TAG_NAME, "tr")
+    mydata = []
+    for row in rows:
+        cells = row.find_elements(By.TAG_NAME, "td")  # Use 'th' for header rows if necessary
+        row_data = [cell.text for cell in cells]
+        mydata.append(row_data)
+    return mydata
+
+
+def prepare_df(mydata):
+    mydf = pd.DataFrame(mydata)
+    mydf.columns = ['date', 'value']
+    mydf = mydf.replace('', None)
+    mydf = mydf.dropna(subset='value').reset_index(drop=True)
+    return mydf
+
+
+def prepare_string_to_float(x):
+    x = x.replace('$', '').replace(',', '.')
+    if '.' in x:
+        pass
+    elif '-' in x:
+        x = '-0.' + x[1:]
+    else:
+        x = '0.' + x
+    return x
+
+
+def download_data(ticker):
+    service = Service(executable_path='chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+
+    # navigating from main page to ticker's revenue
+    main_page_link = 'https://www.macrotrends.net/'
+    driver.get(main_page_link)
+    input_element = driver.find_element(By.CLASS_NAME, 'js-typeahead')
+    input_element.send_keys(ticker)
+    time.sleep(1)
+    input_element.send_keys(Keys.ARROW_DOWN + Keys.ENTER)
+
+    # getting links
+    revenue_link = driver.current_url
+    ticker_link = revenue_link[:-7]
+
+    # scraping revenue data
+    data = get_data_from_table(driver)
+    revenue_df = prepare_df(data)
+
+    # scraping net income data
+    net_income_link = ticker_link + 'net-income'
+    driver.get(net_income_link)
+    data = get_data_from_table(driver)
+    net_income_df = prepare_df(data)
+
+    # shares outstanding
+    shares_outstanding_link = ticker_link + 'shares-outstanding'
+    driver.get(shares_outstanding_link)
+    data = get_data_from_table(driver)
+    shares_outstanding_df = prepare_df(data)
+
+    # joining data
+    df = revenue_df.merge(net_income_df, on='date', how='inner')
+    df = df.merge(shares_outstanding_df, on='date', how='inner')
+    df.columns = ['date', 'Revenue', 'NetIncome', 'Shares']
+    df[['Revenue', 'NetIncome', 'Shares']] = df[['Revenue', 'NetIncome', 'Shares']].map(prepare_string_to_float)
+    df = df.astype({'Revenue': 'float', 'NetIncome': 'float', 'Shares': 'float'})
+    print(df)
+
+    # saving to csv
+    main_folder_path = 'C:\\Users\\barto\\Desktop\\SEC2024\\'
+    raw_data_file_path = f'{main_folder_path}converter\\{ticker}_metrics_raw.csv'
+    df.to_csv(raw_data_file_path, index=False)
+    driver.quit()
+    return df
+
+
+def print_results(df):
+    path = 'C:\\Users\\barto\\Desktop\\SEC2024\\converter\\META_metrics_raw.csv'
+    df = pd.read_csv(path)
+    df['date'] = pd.to_datetime(df['date'])
+
+    fig, axs = plt.subplots(nrows=3, ncols=1, figsize=(14, 10))
+    i = 0
+    for c in df.columns[1:]:
+        sns.lineplot(data=df, x='date', y=c, marker='o', ax=axs[i])
+        i += 1
+    plt.show()
+
+
+def download_data_0(ticker):
+    service = Service(executable_path='chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+    investing_link = ''
+    driver.get(investing_link)
 
 
 ticker = 'META'
-
-driver = webdriver.Chrome()
-
-links = ['https://www.macrotrends.net/stocks/charts/{ticker}/meta-platforms/revenue',
-         'https://www.macrotrends.net/stocks/charts/{ticker}/meta-platforms/shares-outstanding']
-
-for t in list(range(1, 5)):
-    driver.get()
-    driver.execute_script("window.open('');")
-    driver.switch_to.window(driver.window_handles[1])
-    driver.get()
-
-time.sleep(180)
+ddf = download_data(ticker)
+print_results(ddf)
--- pagevaluation.py
+++ pagevaluation.py
@@ -17,24 +17,40 @@ class Valuation:
         self.df = None
 
         self.predicted_pe = None
-        self.predicted_cagr = None
+        self.predicted_net_income_cagr = None
         self.predicted_period = None
         self.net_income_growth_valuation_result = None
         self.net_income_growth_valuation_change = None
+
+        self.predicted_ps = None
+        self.predicted_revenue_cagr  = None
+        self.predicted_rv_period = None
+        self.revenue_growth_valuation_result = None
+        self.revenue_growth_valuation_change = None
 
     def select_ticker(self, ticker):
         self.ticker = ticker
         self.df = self.current_df[self.current_df['Stock'] == ticker].reset_index()
 
     def net_income_growth_valuation_calculation(self):
-        if all(p is not None for p in [self.predicted_pe, self.predicted_cagr, self.predicted_period]):
-            future_net_income = self.df['ttm_NetIncome'] * ((self.predicted_cagr + 1) ** self.predicted_period)
+        if all(p is not None for p in [self.predicted_pe, self.predicted_net_income_cagr, self.predicted_period]):
+            future_net_income = self.df['ttm_NetIncome'] * ((self.predicted_net_income_cagr + 1) ** self.predicted_period)
             future_eps = future_net_income / self.df['Shares']
             self.net_income_growth_valuation_result = (self.predicted_pe * future_eps).round(2)
             self.net_income_growth_valuation_change = (self.net_income_growth_valuation_result / self.df['close'] - 1).round(3)
         else:
             self.net_income_growth_valuation_result = None
             self.net_income_growth_valuation_change = None
+
+    def revenue_growth_valuation_calculation(self):
+        if all(p is not None for p in [self.predicted_ps, self.predicted_revenue_cagr, self.predicted_rv_period]):
+            future_revenue = self.df['ttm_Revenue'] * ((self.predicted_revenue_cagr + 1) ** self.predicted_rv_period)
+            future_rps = future_revenue / self.df['Shares']
+            self.revenue_growth_valuation_result = (self.predicted_ps * future_rps).round(2)
+            self.revenue_growth_valuation_change = (self.revenue_growth_valuation_result / self.df['close'] - 1).round(3)
+        else:
+            self.revenue_growth_valuation_result = None
+            self.revenue_growth_valuation_change = None
 
     def pm_graph_obj(self, columns):
         table_df = self.df[columns]
@@ -63,9 +79,12 @@ def page_valuation(main_folder_path):
             html.Div(html.H3(children='Present metrics')),
             html.Div([
                 html.Div(id='pm_pe', style={'display': 'inline-block'}),
-                html.Div(id='pm_cagr', style={'display': 'inline-block'})
+                html.Div(id='pm_cagr', style={'display': 'inline-block'}),
+                html.Div(id='pm_ps', style={'display': 'inline-block'}),
+                html.Div(id='pm_revenue_cagr', style={'display': 'inline-block'})
             ])
         ]),
+        # PE valuation
         html.Div([
             html.Div(html.H3('Net income growth valuation'), style={'textAlign': 'center'}),
             html.Div([
@@ -90,7 +109,33 @@ def page_valuation(main_folder_path):
                     html.Div(html.H5(id='net_income_growth_valueation_change', style={'height': '1vh', 'padding': 0, 'margin': 0}), style={'display': 'inline-block', 'height': '1vh'})
                 ], style={'textAlign': 'center', 'marginBottom': '2vh'})
             ])
-        ], style={'width': '35vh', 'border': '2px solid black', 'border-radius': '10px'})
+        ], style={'width': '35vh', 'border': '2px solid black', 'border-radius': '10px', 'display': 'inline-block', 'marginRight': '0.5%'}),
+        # PS valuation
+        html.Div([
+            html.Div(html.H3('Revenue growth valuation'), style={'textAlign': 'center'}),
+            html.Div([
+                html.Div([
+                    html.Div(html.H5('Predicted P/S'), style={'display': 'inline-block', 'width': input_label_width, 'height': input_height, 'textAlign': 'right', 'marginRight': '1vh'}),
+                    html.Div(dcc.Input(id='ps_input', type='number', style={'width': '10vh'}), style={'display': 'inline-block', 'width': input_width, 'height': input_height})
+                ], style={'width': '30vh'}),
+                html.Div([
+                    html.Div(html.H5('Predicted revenue growth'), style={'display': 'inline-block', 'width': input_label_width, 'height': input_height, 'textAlign': 'right', 'marginRight': '1vh'}),
+                    html.Div(dcc.Input(id='revenue_cagr_growth_input', type='number', style={'width': '10vh'}), style={'display': 'inline-block', 'width': input_width, 'height': input_height})
+                ], style={'width': '30vh'}),
+                html.Div([
+                    html.Div(html.H5('Time period'), style={'display': 'inline-block', 'width': input_label_width, 'height': input_height, 'textAlign': 'right', 'marginRight': '1vh'}),
+                    html.Div(dcc.Input(id='rv_period_input', type='number', style={'width': '10vh'}), style={'display': 'inline-block', 'width': input_width, 'height': input_height})
+                ], style={'width': '30vh'}),
+                html.Div([
+                    html.Div(html.H5(children='Price:', style={'height': '2vh'}), style={'display': 'inline-block', 'marginRight': '1vh', 'height': '2vh', 'padding': 0, 'margin': 0}),
+                    html.Div(html.H5(id='revenue_growth_valuation_result', style={'height': '2vh'}), style={'display': 'inline-block', 'height': '2vh', 'padding': 0, 'margin': 0})
+                ], style={'textAlign': 'center'}),
+                html.Div([
+                    html.Div(html.H5(children='Change:', style={'height': '1vh', 'padding': 0, 'margin': 0}), style={'display': 'inline-block', 'marginRight': '1vh', 'height': '1vh'}),
+                    html.Div(html.H5(id='revenue_growth_valuation_change', style={'height': '1vh', 'padding': 0, 'margin': 0}), style={'display': 'inline-block', 'height': '1vh'})
+                ], style={'textAlign': 'center', 'marginBottom': '2vh'})
+            ])
+        ], style={'width': '35vh', 'border': '2px solid black', 'border-radius': '10px', 'display': 'inline-block'})
     ])
 
     dash.register_page('ValuationPage', path='/valuation', layout=page_layout)
@@ -99,7 +144,9 @@ def page_valuation(main_folder_path):
         [Output(component_id='ticker_name', component_property='children'),
          Output(component_id='ticker_price', component_property='children'),
          Output(component_id='pm_pe', component_property='children'),
-         Output(component_id='pm_cagr', component_property='children')],
+         Output(component_id='pm_cagr', component_property='children'),
+         Output(component_id='pm_ps', component_property='children'),
+         Output(component_id='pm_revenue_cagr', component_property='children')],
         Input(component_id='dd_tickers', component_property='value'),
         prevent_initial_call=True
     )
@@ -109,8 +156,11 @@ def page_valuation(main_folder_path):
             valuation.select_ticker(value)
             pm_pe_table = valuation.pm_graph_obj(['ttm_P/E', 'ttm_P/E_1y_avg', 'ttm_P/E_3y_avg', 'ttm_P/E_5y_avg'])
             pm_cagr_table = valuation.pm_graph_obj(['NetIncomeCAGR3y', 'NetIncomeCAGR5y'])
-            return valuation.ticker, valuation.df['close'], pm_pe_table, pm_cagr_table
+            pm_ps_table = valuation.pm_graph_obj(['ttm_P/S', 'ttm_P/S_1y_avg', 'ttm_P/S_3y_avg', 'ttm_P/S_5y_avg'])
+            pm_revenue_cagr_table = valuation.pm_graph_obj(['RevenueCAGR3y', 'RevenueCAGR5y'])
+            return valuation.ticker, valuation.df['close'], pm_pe_table, pm_cagr_table, pm_ps_table, pm_revenue_cagr_table
 
+    ############ PE valuation ############
     @app.callback(
         [Output(component_id='net_income_growth_valueation_result', component_property='children', allow_duplicate=True),
          Output(component_id='net_income_growth_valueation_change', component_property='children', allow_duplicate=True)],
@@ -129,9 +179,9 @@ def page_valuation(main_folder_path):
         Input(component_id='cagr_growth_input', component_property='value'),
         prevent_initial_call=True
     )
-    def input_cagr(value):
+    def input_net_income_cagr(value):
         if value is not None:
-            valuation.predicted_cagr = value
+            valuation.predicted_net_income_cagr = value
             valuation.net_income_growth_valuation_calculation()
         return valuation.net_income_growth_valuation_result, valuation.net_income_growth_valuation_change
 
@@ -146,3 +196,41 @@ def page_valuation(main_folder_path):
             valuation.predicted_period = value
             valuation.net_income_growth_valuation_calculation()
         return valuation.net_income_growth_valuation_result, valuation.net_income_growth_valuation_change
+
+############ PS valuation ############
+    @app.callback(
+        [Output(component_id='revenue_growth_valuation_result', component_property='children', allow_duplicate=True),
+         Output(component_id='revenue_growth_valuation_change', component_property='children', allow_duplicate=True)],
+        Input(component_id='ps_input', component_property='value'),
+        prevent_initial_call=True
+    )
+    def input_ps(value):
+        if value is not None:
+            valuation.predicted_ps = value
+            valuation.revenue_growth_valuation_calculation()
+        return valuation.revenue_growth_valuation_result, valuation.revenue_growth_valuation_change
+
+    @app.callback(
+        [Output(component_id='revenue_growth_valuation_result', component_property='children', allow_duplicate=True),
+         Output(component_id='revenue_growth_valuation_change', component_property='children', allow_duplicate=True)],
+        Input(component_id='revenue_cagr_growth_input', component_property='value'),
+        prevent_initial_call=True
+    )
+    def input_revenue_cagr(value):
+        if value is not None:
+            valuation.predicted_revenue_cagr = value
+            valuation.revenue_growth_valuation_calculation()
+        return valuation.revenue_growth_valuation_result, valuation.revenue_growth_valuation_change
+
+    @app.callback(
+        [Output(component_id='revenue_growth_valuation_result', component_property='children', allow_duplicate=True),
+         Output(component_id='revenue_growth_valuation_change', component_property='children', allow_duplicate=True)],
+        Input(component_id='rv_period_input', component_property='value'),
+        prevent_initial_call=True
+    )
+    def input_rv_period(value):
+        if value is not None:
+            valuation.predicted_rv_period = value
+            valuation.revenue_growth_valuation_calculation()
+        return valuation.revenue_growth_valuation_result, valuation.revenue_growth_valuation_change
+
